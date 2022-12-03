d168bf85c93ac680afb145679a07c967a66aa4dc
LucasVera <lucas@lucasdev.info>
2022-12-03 08:54:38 -0500

    fix: yes flag in install script

--- scripts/cypress-dependencies.sh
+++ scripts/cypress-dependencies.sh
@@ -5,4 +5,4 @@ set -e
 
 echo "Installing required cypress dependencies"
 sudo apt update
-sudo apt install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+sudo apt install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
