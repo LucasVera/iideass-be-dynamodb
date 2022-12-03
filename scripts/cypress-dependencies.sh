0db9237343ecc7be629ea554c2312eb6fcb9087c
LucasVera <lucas@lucasdev.info>
2022-12-03 08:50:12 -0500

    fix: cypress dependencies

--- scripts/cypress-dependencies.sh
+++ scripts/cypress-dependencies.sh
@@ -4,4 +4,5 @@
 set -e
 
 echo "Installing required cypress dependencies"
-sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+sudo apt update
+sudo apt install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
