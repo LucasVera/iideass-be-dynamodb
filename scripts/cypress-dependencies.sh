b10aacd1af63b71893128616851794b012481c5a
LucasVera <lucas@lucasdev.info>
2022-12-03 08:43:00 -0500

    fix: cypress dependencies + pipeline improvement

--- scripts/cypress-dependencies.sh
+++ scripts/cypress-dependencies.sh
@@ -4,4 +4,4 @@
 set -e
 
 echo "Installing required cypress dependencies"
-sudo apt install libatk-1.0
+sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
